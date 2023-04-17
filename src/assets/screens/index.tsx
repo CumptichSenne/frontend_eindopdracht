756b33f7c3d0d5aae1bc06484ab81cffee721d63
cumptichsenne <senne.cumptich@student.howest.be>
2023-04-17 16:06:18 +0200

    feat: added tailwind

--- src/assets/screens/index.tsx
+++ src/assets/screens/index.tsx
@@ -1,7 +1,8 @@
 export default () => {
 
  return (
-   <p>Hello world</p>
+  <div className="h-14 bg-gradient-to-r from-sky-500 to-indigo-500">
+   <p className="bg-teal-200 font-bold text-teal-600">Hello wereld</p>
+  </div>
  );
 }
- 
