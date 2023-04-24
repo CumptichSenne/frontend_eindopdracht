e90b9bc38079e699989f03565f25d76bffefa098
cumptichsenne <senne.cumptich@student.howest.be>
2023-04-24 11:40:44 +0200

    feat: added extra info on project page

--- src/assets/screens/content/About.tsx
+++ src/assets/screens/content/About.tsx
@@ -25,8 +25,8 @@ export default () => {
         </div>
         <article className="flex flex-col items-center align-middle justify-center md:flex-row md:items-center">
             <div className="flex flex-col items-center max-w-lg md:basis-3/5 bg-slate-700 md:mr-8 rounded-lg drop-shadow-2xl">
-                <h1 className="text-3xl text-center text-slate-200 max-w-[32rem] font-bold">About me</h1>
-                <p className="text-slate-200 max-w-[32rem] leading-relaxed md:text-lg text-left">
+                <h1 className="text-3xl text-center text-slate-200 max-w-[32rem] font-bold m-4">About me</h1>
+                <p className="text-slate-200 max-w-[32rem] leading-relaxed md:text-lg text-left m-4">
                     Hi there, I'm currently a <span className="font-bold text-yellow-500">student at Howest </span> in Belgium who would like to become a full stack web developper.
                     I have been able to learn a lot of things over the past 2 years that I'm studying and I hope to be able to learn even more in the future.
                 </p>
@@ -38,16 +38,15 @@ export default () => {
                 <img src="src\assets\images\SenneCumptich.jpg" alt="SenneCumptich" className=" h-64 w-36 rounded-xl drop-shadow-2xl"/>
             </div>
         </article>
-        <article className="flex flex-col items-center align-middle justify-center md:flex-row-reverse md:items-center mt-[10vh] m-4">
+        <article className="flex flex-col items-center align-middle justify-center md:flex-row-reverse md:items-center mt-[10vh] mb-10">
             <div className="flex flex-col items-center max-w-lg md:basis-3/5 bg-slate-700 md:mr-8 rounded-lg drop-shadow-2xl">
-                <h1 className="text-3xl text-center text-slate-200 max-w-[32rem] font-bold">Hobbies</h1>
-                <p className="text-slate-200 max-w-[32rem] leading-relaxed md:text-lg text-left">
+                <h1 className="text-3xl text-center text-slate-200 max-w-[32rem] font-bold m-4">Hobbies</h1>
+                <p className="text-slate-200 max-w-[32rem] leading-relaxed md:text-lg text-left m-4">
                     Apart from school I also like to do a lot of other things. One of the main things is playing volleyball.
                     With over 11 years of experience in a club called <span className="font-bold text-yellow-500">Sferos Deinze</span> I never knew that it would be so much fun.
                 </p>
-                <p className="text-slate-200 max-w-[32rem] leading-relaxed md:text-lg mt-4 text-left">
+                <p className="text-slate-200 max-w-[32rem] leading-relaxed md:text-lg mt-4 text-left m-4">
                     When I do have a little bit of free time I also like to draw. I have been drawing for about 3 years now and I really enjoy it because it's a great way to empty your mind.
-
                 </p>
             </div>
             <div className=" flex justify-center lg:basis-1/5 transform transition md:hover:scale-125">
--- src/assets/screens/content/Project.tsx
+++ src/assets/screens/content/Project.tsx
@@ -20,31 +20,141 @@ export default () => {
       </nav>
     </div>
     <div className="flex flex-col align-middle justify-center text-center">
-        <div className="mb-[20vh] mt-[15vh] text-center">
-            <h1 className="text-4xl text-center text-slate-200">Projects</h1>
-        </div>
-        <article className="flex flex-col items-center align-middle justify-center md:flex-row md:items-center">
-            <div className="flex flex-col items-center max-w-lg md:basis-3/5">
-                <h1 className="text-3xl text-center text-slate-200 max-w-[32rem] font-bold">About me</h1>
-                <p className="text-slate-200 max-w-[32rem] leading-relaxed md:text-lg text-left">
-                    Hi there, I'm currently a <span className="font-bold text-yellow-500">student at Howest </span> in Belgium who would like to become a full stack web developper.
-                    I have been able to learn a lot of things over the past 2 years that I'm studying and I hope to be able to learn even more in the future.
-                </p>
-                <p className="text-slate-200 max-w-[32rem] leading-relaxed md:text-lg mt-4 text-left">
-                    When I'm not working for school, you will probably find me with my friends or team members playing <span className="font-bold text-yellow-500">volleyball</span>.
-                    With over 11 years of experience, I have been able to be part of the best team I can imagine and I hope to continue this for a long time.
-                </p>
-                <p className="mt-6 text-center flex justify-center text-slate-200">
-                    Want to know more about me?{' '}
-                    <Link to="/About" className="text-yellow-500 ml-2">
-                    About me
-                    </Link>
-                </p>
+        <div className="mb-[10vh] mt-[5vh] text-center ml-4 mr-4">
+                <h1 className="text-4xl text-center text-slate-200 font-bold">Projects</h1>
             </div>
-            <div className=" flex justify-center lg:basis-1/5 transform transition md:hover:scale-125">
-                <img src="src\assets\images\Sparky.jpg" alt="Sparky" className=" h-64 w-36 rounded-xl drop-shadow-2xl"/>
-            </div>
-        </article>
+            <article className="flex flex-col items-center align-middle justify-center md:flex-row md:items-center m-4 mb-[6vh]">
+                <div className="flex flex-col gap-2">
+                    <div className="flex flex-col items-center max-w-lg md:basis-3/5 bg-slate-700 md:mr-8 rounded-lg drop-shadow-2xl">
+                        <h1 className="text-2xl text-center text-slate-200 max-w-[32rem] font-bold m-4">MCU List</h1>
+                        <p className="text-slate-200 max-w-[32rem] leading-relaxed md:text-lg text-left m-4">
+                            The MCU List is a website that contains all the movies from the <span className="font-bold text-yellow-500">Marvel Cinematic Universe</span>.
+                            Per movie you can find the release date, the director and how much money that specific movie made.
+                        </p>
+                        <p className="text-slate-200 max-w-[32rem] leading-relaxed md:text-lg text-left m-4">
+                            The website is made with <span className="font-bold text-yellow-500">HTML</span> and <span className="font-bold text-yellow-500">CSS</span> and is fully responsive.
+                            For the data I used an open API from Github and with the help of <span className="font-bold text-yellow-500">Javascript</span> I made it possible to display the data on the website.
+                        </p>
+                    </div>
+                    <div className="flex flex-row md:basis-3/5 md:mr-8 max-w-min gap-2">
+                        <div className="bg-slate-700 rounded-lg drop-shadow-2xl">
+                            <p className="text-slate-200 leading-relaxed md:text-lg text-left m-2">
+                                HTML
+                            </p>
+                        </div>
+                        <div className="bg-slate-700 rounded-lg drop-shadow-2xl">
+                            <p className="text-slate-200 leading-relaxed md:text-lg text-left m-2">
+                                CSS
+                            </p>
+                        </div>
+                        <div className="bg-slate-700 rounded-lg drop-shadow-2xl">
+                            <p className="text-slate-200 leading-relaxed md:text-lg text-left m-2">
+                                JS
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className=" flex justify-center lg:basis-1/5 transform transition md:hover:scale-125 mt-8 md:mt-0">
+                    <img src="src\assets\images\interaction.jpg" alt="Interaction Design" className="h-80 w-64 rounded-xl drop-shadow-2xl"/>
+                </div>
+            </article>
+            <article className="flex flex-col items-center align-middle justify-center md:flex-row-reverse md:items-center m-4 mb-[6vh]">
+                <div className="flex flex-col gap-2">
+                    <div className="flex flex-col items-center max-w-lg md:basis-3/5 bg-slate-700 md:ml-8 rounded-lg drop-shadow-2xl">
+                        <h1 className="text-2xl text-center text-slate-200 max-w-[32rem] font-bold m-4">Smart Pool</h1>
+                        <p className="text-slate-200 max-w-[32rem] leading-relaxed md:text-lg text-left m-4">
+                            Smart Pool is a project which can be used to get all kinds of information about <span className="font-bold text-yellow-500">the water in your pool</span>.
+                            You can get the temperature of the water, how clean your water is and the PH value.
+                        </p>
+                    </div>
+                    <div className="flex flex-row md:basis-3/5 md:ml-8 max-w-min gap-2">
+                        <div className="bg-slate-700 rounded-lg drop-shadow-2xl">
+                            <p className="text-slate-200 leading-relaxed md:text-lg text-left m-2">
+                                HTML
+                            </p>
+                        </div>
+                        <div className="bg-slate-700 rounded-lg drop-shadow-2xl">
+                            <p className="text-slate-200 leading-relaxed md:text-lg text-left m-2">
+                                CSS
+                            </p>
+                        </div>
+                        <div className="bg-slate-700 rounded-lg drop-shadow-2xl">
+                            <p className="text-slate-200 leading-relaxed md:text-lg text-left m-2">
+                                JS
+                            </p>
+                        </div>
+                        <div className="bg-slate-700 rounded-lg drop-shadow-2xl">
+                            <p className="text-slate-200 leading-relaxed md:text-lg text-left m-2">
+                                C++
+                            </p>
+                        </div>
+                        <div className="bg-slate-700 rounded-lg drop-shadow-2xl">
+                            <p className="text-slate-200 leading-relaxed md:text-lg text-left m-2">
+                                MySQL
+                            </p>
+                        </div>
+                        <div className="bg-slate-700 rounded-lg drop-shadow-2xl">
+                            <p className="text-slate-200 leading-relaxed md:text-lg text-left m-2">
+                                Python
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col justify-center lg:basis-1/5 gap-8 mt-8 md:mt-0">
+                    <div className="transform transition md:hover:scale-125">
+                        <img src="src\assets\images\projectone_1.jpg" alt="Project one" className="w-56 h-32 rounded-xl drop-shadow-2xl"/>
+                    </div>
+                    <div className="transform transition md:hover:scale-125">
+                        <img src="src\assets\images\projectone_2.jpg" alt="Project one" className="w-56 h-32 rounded-xl drop-shadow-2xl"/>
+                    </div>
+                </div>
+            </article>
+            <article className="flex flex-col items-center align-middle justify-center md:flex-row md:items-center m-4 mb-[6vh]">
+                <div className="flex flex-col gap-2">
+                    <div className="flex flex-col items-center max-w-lg md:basis-3/5 bg-slate-700 md:mr-8 rounded-lg drop-shadow-2xl">
+                        <h1 className="text-2xl text-center text-slate-200 max-w-[32rem] font-bold m-4">Catch the fish</h1>
+                        <p className="text-slate-200 max-w-[32rem] leading-relaxed md:text-lg text-left m-4">
+                            This is my most recent project that I made for school where you can play up to 3 different games.
+                            The user can choose between Red vs Blue, Simon says and Zen. During the chosen game the player has to push buttons that are lit up with LED's.
+                        </p>
+                    </div>
+                    <div className="flex flex-row md:basis-3/5 md:mr-8 max-w-min gap-2">
+                        <div className="bg-slate-700 rounded-lg drop-shadow-2xl">
+                            <p className="text-slate-200 leading-relaxed md:text-lg text-left m-2">
+                                HTML
+                            </p>
+                        </div>
+                        <div className="bg-slate-700 rounded-lg drop-shadow-2xl">
+                            <p className="text-slate-200 leading-relaxed md:text-lg text-left m-2">
+                                CSS
+                            </p>
+                        </div>
+                        <div className="bg-slate-700 rounded-lg drop-shadow-2xl">
+                            <p className="text-slate-200 leading-relaxed md:text-lg text-left m-2">
+                                JS
+                            </p>
+                        </div>
+                        <div className="bg-slate-700 rounded-lg drop-shadow-2xl">
+                            <p className="text-slate-200 leading-relaxed md:text-lg text-left m-2">
+                                C++
+                            </p>
+                        </div>
+                        <div className="bg-slate-700 rounded-lg drop-shadow-2xl">
+                            <p className="text-slate-200 leading-relaxed md:text-lg text-left m-2">
+                                MySQL
+                            </p>
+                        </div>
+                        <div className="bg-slate-700 rounded-lg drop-shadow-2xl">
+                            <p className="text-slate-200 leading-relaxed md:text-lg text-left m-2">
+                                C#
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className=" flex justify-center lg:basis-1/5 md:transform md:transition md:hover:scale-125 mt-8 md:mt-0">
+                    <img src="src\assets\images\teamproject.jpg" alt="Team project" className="h-64 w-64 rounded-xl drop-shadow-2xl"/>
+                </div>
+            </article>
     </div>
     <footer className="flex flex-col items-center justify-center pb-4 text-slate-300">
         <p>
